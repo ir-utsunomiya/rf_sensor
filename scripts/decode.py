8e9f0cdc3d38e0b4b2ae0a7355ac326e74d9310c
RMiyagusuku <miyagusuku@robot.t.u-tokyo.ac.jp>
2019-07-01 20:49:06 +0900

    adding time to retrieved information from pcap

--- scripts/decode.py
+++ scripts/decode.py
@@ -6,17 +6,20 @@ def tcp2matrix(stream):
 
     Rss = list()
     for l in stream.readlines():
-        #print(l)
+        print(l)
         w = str(l).split()
         rss = list()
+        t_s = w[0].split(':')
+        t_s = int(t_s[0])*3600+int(t_s[1])*60+float(t_s[2]) 
+                    
         for i in range(len(w)):
             if 'MHz' in w[i]: freq = int(w[i-1]) 
             if 'dBm' in w[i]: rss.append(int(w[i].split('dBm')[0]))
             if 'Beacon' in w[i]: mac = w[i+1]    
-        Rss.append([freq,mac,np.asarray(rss)])
+        Rss.append([t_s,freq,mac,np.asarray(rss)])
  
     for rss in Rss:
-        print('{:4d}, {:20s}, {:}'.format(*rss))
+        print('{:6f}, {:4d}, {:20s}, {:}'.format(*rss))
 
     return Rss
     
