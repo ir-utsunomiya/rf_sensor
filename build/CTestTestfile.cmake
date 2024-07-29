8640704787a034ca424b953fd63d2f55d739ffdb
Your name <t212304@s.utsunomiya-u.ac.jp>
2024-07-29 10:24:04 +0900

    add rss2vec_maxrss

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nanakubo/catkin_ws/src/rf_sensor
-# Build directory: /home/nanakubo/catkin_ws/src/rf_sensor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
