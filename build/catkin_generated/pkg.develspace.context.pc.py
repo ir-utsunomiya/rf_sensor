3b2c2adca8500aa0ed63df18de902ccf21fe6b3a
Your name <t212304@s.utsunomiya-u.ac.jp>
2024-07-29 10:23:40 +0900

    add rss2vec_maxrss

---
+++ build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "rospy;std_msgs;rf_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "rf_sensor"
+PROJECT_SPACE_DIR = "/home/nanakubo/catkin_ws/src/rf_sensor/build/devel"
+PROJECT_VERSION = "0.0.0"
