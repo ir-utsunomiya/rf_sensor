2e0ed9451e7265cd7e304839657fafc8ca8cc4f4
kawauchi0923 <kawauchi@irlab-uu.jp>
2025-05-20 11:36:20 +0900

    modified directional_strength_test.cpp

--- wifi/directional_strength_test.cpp
+++ wifi/directional_strength_test.cpp
@@ -6,25 +6,17 @@
 #include <unistd.h>
 
 #include <ros/ros.h>
-#include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"  // Float32型に変更
 
 float rate = 10;
-int off_flag = 0;
-
-std::atomic<int> on_flag(0);
 std::atomic<int> input_count(0);
-
 
 void waitEnter()
 {
     while (ros::ok())
     {
-        std::cin.get();
+        std::cin.get();  // Enter入力待ち
         input_count++;
-        if (input_count % 2 == 1)
-        {
-            on_flag++;
-        }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
@@ -33,25 +25,26 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "directional_strength_test");
     ros::NodeHandle nh;
 
-    ros::Publisher pub = nh.advertise<std_msgs::Int32>("angle_flag", 10);
+    ros::Publisher pub = nh.advertise<std_msgs::Float32>("angle_flag", 10);
     ros::Rate r(rate);
-    std_msgs::Int32 flag;
+    std_msgs::Float32 flag;
 
     std::thread inputThread(waitEnter);
 
     while (ros::ok())
     {
-        int judgment = input_count % 2;
+        int count = input_count.load();
 
-        switch (judgment)
+        if (count % 2 == 0)
         {
-            case 0:
-                flag.data = off_flag;
-                break;
-            case 1:
-                flag.data = on_flag;
-                break;
+            flag.data = 0.0;
         }
+        else
+        {
+            // (count - 1) / 2 = 0,1,2,3,... → 1.0, 1.5, 2.0, ...
+            flag.data = 1.0 + 0.5f * ((count - 1) / 2);
+        }
+
         std::cout << "angle_flag : " << flag.data << std::endl;
         pub.publish(flag);
         r.sleep();
